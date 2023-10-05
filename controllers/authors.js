48f591a708580b8ee495ec144d1b2fb979743600
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-10-05 17:52:43 -0400

    authors model setup completed

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+
+//Index Router
 router.get('/', (req, res) => {
     res.render('authors/index.ejs')
+})
+
+//New Router
+router.get('/new', (req, res) => {
+    res.render('authors/new.ejs')
 })
 
 module.exports = router;

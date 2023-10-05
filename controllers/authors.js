f4eab0c206a46a1a17c47145fbe86891fbee424f
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-10-05 19:39:25 -0400

    controllers updated

--- controllers/authors.js
+++ controllers/authors.js
@@ -13,4 +13,16 @@ router.get('/new', (req, res) => {
     res.render('authors/new.ejs')
 })
 
+
+//Create route.
+router.post('/', (req, res) => {
+    Author.create(req.body)
+        .then((createdAuthor) => {
+            res.redirect('/authors')
+        })
+        .catch((err) => {
+            res.status(501).json(err.message)
+        })
+})
+
 module.exports = router;
